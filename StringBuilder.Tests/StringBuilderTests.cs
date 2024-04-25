663d807e7487019a0c70881ad69cf81d00c122d8
Daniel Foord <dfoord@bscglobal.com>
2024-04-25 21:03:09 +0200

    Implemented some tests

--- StringBuilder.Tests/StringBuilderTests.cs
+++ StringBuilder.Tests/StringBuilderTests.cs
@@ -3,8 +3,31 @@ namespace StringBuilder.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void GivenString_WhenCallingAppend_ThenStringGetsAppendedToTheEndOfTheState()
     {
+        // Arrange
+        var sb = new StringBuilder();
 
+        // Act
+        sb.Append("Test");
+        sb.Append("String");
+
+        // Assert
+        Assert.Equal("TestString", sb.ToString());
+    }
+
+    [Fact]
+    public void GivenCharacter_WhenCallingAppend_ThenCharacterGetsAppendedToTheEndOfTheState()
+    {
+        // Arrange
+        var sb = new StringBuilder();
+
+        // Act
+        sb.Append("Test");
+        sb.Append("String");
+        sb.Append('!');
+
+        // Assert
+        Assert.Equal("TestString!", sb.ToString());
     }
 }
