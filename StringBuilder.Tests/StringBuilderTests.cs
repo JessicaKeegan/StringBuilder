94c1cc3ea112b5570dde5c13864b4bc55d122b21
Daniel Foord <dfoord@bscglobal.com>
2024-04-25 21:17:44 +0200

    AppendLine test

--- StringBuilder.Tests/StringBuilderTests.cs
+++ StringBuilder.Tests/StringBuilderTests.cs
@@ -3,7 +3,7 @@ namespace StringBuilder.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void GivenString_WhenCallingAppend_ThenStringGetsAppendedToTheEndOfTheState()
+    public void Given_String_When_CallingAppend_Then_StringGetsAppendedToTheEndOfTheState()
     {
         // Arrange
         var sb = new StringBuilder();
@@ -17,7 +17,7 @@ public class UnitTest1
     }
 
     [Fact]
-    public void GivenCharacter_WhenCallingAppend_ThenCharacterGetsAppendedToTheEndOfTheState()
+    public void Given_Character_When_CallingAppend_Then_CharacterGetsAppendedToTheEndOfTheState()
     {
         // Arrange
         var sb = new StringBuilder();
@@ -29,5 +29,23 @@ public class UnitTest1
 
         // Assert
         Assert.Equal("TestString!", sb.ToString());
+    }
+
+
+    [Fact]
+    public void Given_String_When_CallingAppendLine_Then_CharacterGetsAppendedToTheEndOfTheStateWithANewLine()
+    {
+        // Arrange
+        var sb = new StringBuilder();
+
+        // Act
+        sb.Append("Test");
+        sb.Append("String");
+        sb.Append('!');
+        sb.AppendLine("Some extra content");
+        sb.AppendLine("And another new line");
+
+        // Assert
+        Assert.Equal("TestString!\r\nSome extra content\r\nAnd another new line", sb.ToString());
     }
 }
