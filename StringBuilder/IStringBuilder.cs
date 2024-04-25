0deb60a13ea8e3057bbbd3dd9467ecdc1257d16d
Daniel Foord <dfoord@bscglobal.com>
2024-04-25 20:53:03 +0200

    Added some more methods to IStringBuilder

--- StringBuilder/IStringBuilder.cs
+++ StringBuilder/IStringBuilder.cs
@@ -25,6 +25,26 @@ public interface IStringBuilder
     void AppendLine(string value);
 
     /// <summary>
+    /// Return the index of the first occourence of the specified substring
+    /// </summary>
+    /// <example>
+    ///
+    /// var sb = new StringBuilder();
+    /// sb.Append("This is an example string");
+    /// Console.WriteLine(sb.FirstIndexOf("ample")); // Prints 13
+    ///
+    /// </example>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    int FirstIndexOf(String value);
+
+    /// <summary>
+    /// Return a new instance of a <c>StringBuilder</c> with the state reversed
+    /// </summary>
+    /// <param name="value">The <c>string</c> to append as a new line to the state</param>
+    StringBuilder Reverse(string value);
+
+    /// <summary>
     /// Get the state of the builder as a <c>string</c>
     /// </summary>
     /// <returns>The current built up <c>string</c></returns>
--- StringBuilder/StringBuilder.cs
+++ StringBuilder/StringBuilder.cs
@@ -2,25 +2,36 @@
 
 public class StringBuilder : IStringBuilder
 {
-    /// <inheritdoc>
+    /// <inheritdoc/>
     public void Append(string value)
     {
         throw new NotImplementedException();
     }
 
-    /// <inheritdoc>
+    /// <inheritdoc/>
     public void Append(char character)
     {
         throw new NotImplementedException();
     }
 
-    /// <inheritdoc>
+    /// <inheritdoc/>
     public void AppendLine(string value)
     {
         throw new NotImplementedException();
     }
 
-    /// <inheritdoc>
+    /// <inheritdoc/>
+    public int FirstIndexOf(string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public StringBuilder Reverse(string value)
+    {
+        throw new NotImplementedException();
+    }
+
     public override string ToString()
     {
         throw new NotImplementedException();
